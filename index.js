7e927ed6f2ce498783f39ccd708be32969346d28
arnabsrj <srjglobal.arnab.fullstack@gmail.com>
2025-07-26 16:00:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // ✅ CORS Configuration
 app.use(cors({
-    origin: "https://frontend-varnabs-projects.vercel.app",  // ✅ Allow all origins
+    origin: "*",  // ✅ Allow all origins
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],  
       credentials: true,    
